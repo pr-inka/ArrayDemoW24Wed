b18c3763a346077657364f83fab11e469ac8c7f2
Prinka Prinka <prinka@sheridancollege.ca>
2024-01-24 09:13:37 -0500

    second commit: student.java file, added commit

---
+++ src/arrydemo/student.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arrydemo;
+
+/**
+ *
+ * @author prink
+ */
+public class student {
+    private String sname;
+    private String Address;
+    private String  getAddress() {
+        return Address;
+    }
+    
+    public void setAddress (String Address) {
+        this.Address = Address;
+    }
+    
+    
+}
